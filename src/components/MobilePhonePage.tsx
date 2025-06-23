e31322a1e3885f04bbf3b1285f414eecf6690d0d
Muhammad Kashif <60599437+mhdkashif07@users.noreply.github.com>
2025-06-23 16:21:20 +0500

    added image

--- src/components/MobilePhonePage.tsx
+++ src/components/MobilePhonePage.tsx
@@ -18,19 +18,6 @@ const MobilePhonePage = ({
 
   return (
     <>
-      <div
-        className="brands-banner dark-background responsive-image"
-        data-aos="fade"
-        style={{ backgroundImage: "url(/img/mobile-banner.webp)" }}
-      ></div>
-      <div
-        className="brands-banner dark-background responsive-image-sm"
-        data-aos="fade"
-        style={{
-          backgroundImage: "url(/img/Banner-Mobile-02.jpg)",
-          height: "480px",
-        }}
-      ></div>
       <main className="p-6">
         <h1 className="text-xl font-semibold mb-4">{title}</h1>
         <div
