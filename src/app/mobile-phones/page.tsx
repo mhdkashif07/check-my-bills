099ab93c86e1b138a9ffb9686d533f118b619758
Muhammad Kashif <60599437+mhdkashif07@users.noreply.github.com>
2025-06-24 10:41:17 +0500

    small fix

--- src/app/mobile-phones/page.tsx
+++ src/app/mobile-phones/page.tsx
@@ -2,8 +2,6 @@
 
 import React from "react";
 import { useStickeeWidget } from "@/hooks/useStickeeWidget";
-// import Image from "next/image";
-import Loader from "@/components/Loader";
 
 declare global {
   interface Window {
@@ -16,16 +14,10 @@ export default function MobilePhones() {
 
   return (
     <>
-      {/* <Image
-        src="/mobile-banner.svg"
-        alt="Mobile Banner"
-        className="w-full h-auto px-6 py-3"
-        width={200}
-        height={200}
-      /> */}
-      <div data-stickee-widget-id="check-my-bills-mobile-504">
-        <Loader />
-      </div>
+      <main className="p-6">
+        <h1 className="text-xl font-semibold mb-4">Check My Bills</h1>
+        <div data-stickee-widget-id="check-my-bills-mobile-504">Loading...</div>
+      </main>
     </>
   );
 }
